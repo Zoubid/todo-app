5f5573444f47f4738ef022fef0c125cd1da52fde
zoubid <empressbida@gmail.com>
2017-08-08 11:16:08 +0100

    []:

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+	'name' : "To-Do application",
+	
+	'description' : "MAnage your personal tasks",
+	
+	'summary' : """ gestion des task """,
+	
+	'author' : "SAlah zoubida", 
+	
+	'depends' : ['mail'],
+	
+	'website' : "www.zoubida.com",
+	
+	'data' : ['todoview.xml',
+           # 'security/ir.model.access.csv'
+			  ],
+}
